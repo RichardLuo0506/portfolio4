6e7f2f9a84fabfd9d9aae287e00151b0b137c57a
Richard Luo <richardluo0506@gmail.com>
2019-09-16 00:13:43 -0700

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.sass']
 })
+
 export class AppComponent {
-  title = 'portfolio4';
+
 }
